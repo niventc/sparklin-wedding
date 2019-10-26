f46a324408b8226629b472e0c09a54741067ca1f
Vincent Sparkes <niventc@gmail.com>
2019-10-26 19:07:33 +0100

    Add thanks page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,9 +10,11 @@ export class HomeComponent implements OnInit {
   public password = "";
   public isDayGuest = false;
   public isEveningGuest = false;
+  public isThanksGuest = false;
 
   private daytimePassword = "c3Vuc2hpbmU=";
   private eveningPassword = "bW9vbmxpZ2h0";
+  private photoPassword = "dGhhbmtz";
 
   constructor() { }
 
@@ -24,17 +26,17 @@ export class HomeComponent implements OnInit {
       this.isDayGuest = true;
     } else if (btoa(password) === this.eveningPassword) {
       this.isEveningGuest = true;
+    } else if (btoa(password) === this.photoPassword) {
+      this.isThanksGuest = true;
     }
   }
-
 }
-
 
 window.onscroll = () => {
   const container = document.getElementById("container");
   const content = document.getElementById("content");
 
-  if (window.pageYOffset + 150 > content.offsetTop) {
+  if (content && window.pageYOffset + 150 > content.offsetTop) {
     container.classList.add("sticky");
   } else {
     container.classList.remove("sticky");
